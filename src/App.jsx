4c786e781fd789c329701228ffc2ad63970258c6
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-11-27 02:39:50 +0700

    create weather app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { WeatherApp } from "./Components/WeatherApp/WeatherApp";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <WeatherApp />
+    </>
+  );
+}
+
+export default App;
